fbc4be019b25b8a4d04fa0d887c1fec20db0d931
Iamsushmithagowda <sushmithagowda.2004s@gmail.com>
2026-05-05 22:54:04 +0530

    Initial CI/CD project

---
+++ test.js
@@ -0,0 +1,10 @@
+console.log("Running tests...");
+
+// simple test example
+const result = 2 + 2;
+
+if (result !== 4) {
+    throw new Error("Test failed!");
+}
+
+console.log("All tests passed!");
